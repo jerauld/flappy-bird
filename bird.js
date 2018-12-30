2f1fb2475fa1724f64af006afd7ef70060eb2750
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-12-29 21:09:30 -0800

    Update bird.js
    Editing on mobile is difficult. Latest bird object.

--- bird.js
+++ bird.js
@@ -1,21 +1,26 @@
 function Bird() {
+    // Bird Dimensions
     this.y = height/2;
     this.x = 64;
 
+    // Physics
     this.gravity = 0.6;
     this.lift = -20;
     this.velocity = 0;
 
+    // Show Bird
     this.show = function() {
-        fill(255);
+        fill(22,177,181);
         ellipse(this.x, this.y, 32, 32);
     }
 
+    // Upforce
     this.up = function() {
         this.velocity += this.lift;
         
     }
 
+    // Physics
     this.update = function() {
         this.velocity += this.gravity;
         this.velocity *= 0.9;
